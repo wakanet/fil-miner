f7372f7425d72c5a048e82d74a55ff092af657f0
free1139@163.com <free1139@163.com>
2022-08-14 10:51:23 +0000

    add publish custom shell

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rel_dir="./publish/fil-miner"
+
+rm -rf $rel_dir 
+mkdir -p $rel_dir
+cp -r ./apps $rel_dir
+cp -r ./bin $rel_dir
+cp -r ./etc $rel_dir
+cp -r ./doc $rel_dir
+cp -r ./script $rel_dir
+cp -r README.md $rel_dir
+cp -r env.sh $rel_dir
+cp -r version $rel_dir
+
+# clean the source
+rm -r $rel_dir/etc/supd/apps/*.ini
