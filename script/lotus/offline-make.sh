60bdbaa7efad544be06c9610d98e35e43f672950
free1139 <free1139@163.com>
2022-12-26 00:19:14 -0500

    fix offline maker

--- script/lotus/offline-make.sh
+++ script/lotus/offline-make.sh
@@ -3,4 +3,4 @@
 addr="t12n3tcxv4gsnpkfdq5qnbleje4gsmrmfwpfp4nia"
 
 #./miner.sh storage-deals set-ask --price 0.0000000 --verified-price 0.0000000 --min-piece-size 256B --max-piece-size 2KiB
-./miner.sh storage-deals offline-make --from $addr /tmp/files1.txt
+./miner.sh storage-deals offline-make --from $addr --src-max-size=128 --pack-interval=3 /tmp/files.txt
