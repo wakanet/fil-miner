19873cc23ca84841027be14b998cc455552439e1
free1139 <free1139@163.com>
2023-01-01 18:56:45 -0500

    fix netip

--- apps/lotus/ip.sh
+++ apps/lotus/ip.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 getip() {
+    defaultIP="127.0.0.1"
     ips=$(ip a | grep -Po '(?<=inet ).*(?=\/)'|grep -E "^10\.") # only support one eth card.
     for ip in $ips
     do
-        echo $ip
+        defaultIP=$ip
         break
     done
-    echo "127.0.0.1"
+    echo $defaultIP
 }
 echo $(getip)
