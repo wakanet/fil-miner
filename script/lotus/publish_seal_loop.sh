a9bc5f52eed4818c251b8bf1ba81d1a76feb2857
free1139 <free1139@163.com>
2023-01-03 23:34:06 -0500

    add auto nonce fix

--- script/lotus/publish_seal_loop.sh
+++ script/lotus/publish_seal_loop.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+addr="f3vxtiecaaydmjjqmvp5wbwxledzhauatgz5ciuuyn2kb3vjnrp6precafblqpv3vro4mlqzovbftgr5w7or2a"
+
 while true
 do
   echo $(date --rfc-3339=ns)
@@ -7,4 +9,10 @@ do
   ./miner.sh sectors batching commit --publish-now
   echo "sleep 600"
   sleep 600
+  mpoolCur=$(./health.sh chain-mpool)
+  if [ $mpoolCur -gt 30 ]; then
+     echo "basefee is too hight, try rise the gas"
+     ./lotus.sh mpool stat --local
+     ./lotus.sh mpool fstar-fix --limit-msg=30 $addr
+  fi
 done
