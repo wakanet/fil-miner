b7d8b9ed3b1befdd9d8a0f9adb04acdc0e8f4bec
Shu <free1139@163.com>
2023-01-06 09:26:38 +0000

    publish seal loop

--- script/lotus/publish_seal_loop.sh
+++ script/lotus/publish_seal_loop.sh
@@ -5,14 +5,15 @@ addr="f3vxtiecaaydmjjqmvp5wbwxledzhauatgz5ciuuyn2kb3vjnrp6precafblqpv3vro4mlqzov
 while true
 do
   echo $(date --rfc-3339=ns)
+  ./miner.sh storage-deals pending-publish --publish-now
   ./miner.sh sectors batching precommit --publish-now
   ./miner.sh sectors batching commit --publish-now
   echo "sleep 600"
   sleep 600
   mpoolCur=$(./health.sh chain-mpool)
-  if [ $mpoolCur -gt 30 ]; then
+  if [ $mpoolCur -gt 2 ]; then
      echo "basefee is too hight, try rise the gas"
      ./lotus.sh mpool stat --local
-     ./lotus.sh mpool fstar-fix --limit-msg=30 $addr
+     ./lotus.sh mpool fstar-fix --really-do-it --limit-msg=0 $addr
   fi
 done
