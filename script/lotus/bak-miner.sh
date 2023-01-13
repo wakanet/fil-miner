fccede4392c320519351ed23c133825237120dc9
Shu <free1139@163.com>
2023-01-13 04:13:49 +0000

    add miner repo backup

---
+++ script/lotus/bak-miner.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+src="/data/sdb/lotus-user-1/.lotusminer"
+dest="/data/sdb/lotus-user-1/bak" # need mount the remote storage by manually
+while true
+do
+    echo $(date --rfc-3339=ns)
+    rsync -Pat $src $dest
+    echo $(date --rfc-3339=ns)
+    echo "sleep 60"
+    sleep 60
+done
+
