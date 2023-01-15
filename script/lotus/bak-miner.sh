6e74c0e6983dd63f0763cd0f8904757e6e2b727c
free1139 <free1139@163.com>
2023-01-14 22:13:26 -0500

    fix bak-miner

--- script/lotus/bak-miner.sh
+++ script/lotus/bak-miner.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 src="/data/sdb/lotus-user-1/.lotusminer"
-dest="/data/sdb/lotus-user-1/bak" # need mount the remote storage by manually
+dest="/data/nfs/lotus-user-1/bak" # need mount the remote storage by manually
 while true
 do
     echo $(date --rfc-3339=ns)
-    rsync -Pat $src $dest
+    rsync -Pat --exclude="deal-staging" --exclude="dagstore" --exclude="journal" $src $dest
     echo $(date --rfc-3339=ns)
     echo "sleep 60"
     sleep 60
