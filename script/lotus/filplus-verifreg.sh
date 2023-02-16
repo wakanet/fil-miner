74d44a3616585f88e8204df483deafab47e69c8e
free1139@163.com <free1139@163.com>
2023-02-16 03:11:59 +0000

    fix shell script

--- script/lotus/filplus-verifreg.sh
+++ script/lotus/filplus-verifreg.sh
@@ -7,5 +7,10 @@ if [ -z $verifer ]; then
     exit
 fi
 
-./shed.sh verifreg add-verifier t14po2vrupy7buror4g55c7shlcrmwsjxbpss7dzy $verifer 1024000
+if [ -z "$lotusrepo" ]; then
+    echo "Not found env 'lotusrepo'"
+    exit 0
+fi
+
+sh -x ./shed.sh verifreg --repo=$lotusrepo add-verifier t14po2vrupy7buror4g55c7shlcrmwsjxbpss7dzy $verifer 1024000
 
--- script/lotus/shed.sh
+++ script/lotus/shed.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 export FIL_PROOFS_PARAMETER_CACHE="/data/cache/filecoin-proof-parameters/v28" 
 
-if [ -z "$lotusrepo" ]; then
-    echo "Not found env 'lotusrepo'"
-    exit 0
-fi
-if [ -z "$filrepo" ]; then
-    echo "Not found env 'filrepo'"
-    exit 0
-fi
-
-sudo $PRJ_ROOT/apps/lotus/lotus-shed --log-level=debug --repo=$lotusrepo --miner-repo=$filrepo "$@"
+sudo $PRJ_ROOT/apps/lotus/lotus-shed --log-level=debug "$@"
 
