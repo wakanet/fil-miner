b294f5d0bd4fef40676092e51be23698ba175236
free1139 <free1139@163.com>
2023-04-26 13:07:10 +0000

    fix dc deal

---
+++ script/lotus/dc_nfs_loop.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+# Doc:
+# import csv
+# sqlite3 task.db
+# .separator ","
+# .import ./task.csv market_task
+
+storage_id=1
+
+sleep_sec=20
+call_sleep(){
+  echo "sleep "$sleep_sec
+  sleep $sleep_sec
+}
+
+while true
+do
+    echo $(date --rfc-3339=ns)
+    call_sleep
+
+    idle=$(./miner.sh fstar-worker producer-idle)
+    isIdle=$(echo "$idle"|sed -n "/^[0-9][0-9]*$/p")
+    apLimit=$(./miner.sh pledge-sector get-limit-ap-cur)
+    isAPLimit=$(echo "$apLimit"|sed -n "/^[0-9][0-9]*$/p")
+    diskUsed=$(df /data --output=pcent|sed '1d'|sed 's/%//g'|awk '{printf $1}')
+
+    if [ -z "$isIdle" ];then
+            echo "idle failed, idle:$idle,limit:$apLimit,disk:$diskUsed"
+            continue
+    fi
+#    if [ $idle -lt 1 ]; then
+#            echo "no idle, idle:$idle,limit:$apLimit,disk:$diskUsed"
+#            continue
+#    fi
+    if [ $diskUsed -gt 97 ]; then
+            echo "disk failed, idle:$idle,limit:$apLimit,disk:$diskUsed"
+            continue
+    fi
+    if [ -z "$isAPLimit" ];then
+            echo "ap limit not a number, idle:$idle,limit:$apLimit,disk:$diskUsed"
+            continue
+    fi
+#    idleLimit=$(expr $idle - $apLimit)
+    #if [ $idleLimit -lt 0 ]; then
+    if [ $apLimit -gt 100 ]; then
+            echo "apLimit more then the idles, idle:$idle,limit:$apLimit,disk:$diskUsed"
+            continue
+    fi
+
+    # fetch data
+    ID=$(echo "SELECT ID FROM market_task WHERE State=0 LIMIT 1"|sqlite3 task.db)
+    if [ -z "$ID" ]; then
+      echo "no data"
+      continue
+    fi
+    propCid=$(echo "SELECT UUID FROM market_task WHERE ID=$ID"|sqlite3 task.db)
+    rootCid=$(echo "SELECT PayloadCid FROM market_task WHERE ID=$ID"|sqlite3 task.db)
+    pieceCid=$(echo "SELECT PieceCID FROM market_task WHERE ID=$ID"|sqlite3 task.db)
+    pieceSize=$(echo "SELECT PieceSize FROM market_task WHERE ID=$ID"|sqlite3 task.db)
+    clientAddr=$(echo "SELECT Client FROM market_task WHERE ID=$ID"|sqlite3 task.db)
+    filePath=$pieceCid".car"
+    
+    echo "ID:$ID,propCid:$propCid,rootCid:$rootCid,pieceCid:$pieceCid,pieceSize:$pieceSize,clientAddr:$clientAddr,car:$filePath"
+    echo "UPDATE market_task SET State=1 WHERE ID=$ID"|sqlite3 task.db
+
+    echo "make propose, idle:$idle,limit:$apLimit,disk:$diskUsed"
+    echo $(date --rfc-3339=ns)
+    if [ -z "$propCid" ]; then
+      echo "cid not found: $propose_out"
+      exit
+    fi
+    
+    output=$(./miner.sh fstar-market new-fstmp --really-do-it)
+    filename=$(basename $output)
+    remoteUri=$filePath
+    touch $output # make a empty fsmtp
+    
+    echo "record-deal:"$propCid" "$output
+    echo $(date --rfc-3339=ns)
+    ./miner.sh fstar-market record-deal $propCid $rootCid $pieceCid $pieceSize $clientAddr $output $remoteUri $storage_id
+    if [ $? -ne 0 ]; then
+      echo "record failed: $propose_out"
+      continue
+    fi
+    
+    echo "import-data:"$propCid" "$output
+    echo $(date --rfc-3339=ns)
+    ./miner.sh storage-deals import-data --storage-id=$storage_id $propCid $output $remoteUri
+    if [ $? -ne 0 ]; then
+      echo "import failed: $propose_out"
+      continue
+    fi
+    echo "end"
+done
--- script/lotus/publish_seal_loop.sh
+++ script/lotus/publish_seal_loop.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-addr="f3vxtiecaaydmjjqmvp5wbwxledzhauatgz5ciuuyn2kb3vjnrp6precafblqpv3vro4mlqzovbftgr5w7or2a"
+addr=$1
+count=0
+
+if [ -z "$addr" ]; then
+    addr="f3rhyjgokmwicrvpzsaj3pqlorlxx56xlvx5uwiaf4ljbwostwlxh6gbzl76yd2h5lljreb6j4qzpmp3hocboq"
+fi
 
 while true
 do
@@ -8,12 +13,29 @@ do
   ./miner.sh storage-deals pending-publish --publish-now
   ./miner.sh sectors batching precommit --publish-now
   ./miner.sh sectors batching commit --publish-now
-  echo "sleep 600 or ctrl+c exit"
-  sleep 600
-  mpoolCur=$(./health.sh chain-mpool)
+
+
+  echo "sleep 100 to waitting message publish, or ctrl+c exit, count:$count"
+  sleep 100
+  ./lotus.sh mpool fstar-fix --only-feecap=true --rate-feecap=100000 --rate-limit=10000 --limit-feecap=0.00000001FIL --max-gas=10FIL --limit-msg=0 --really-do-it $addr
+
+  count=$(($count+1))
+  echo "sleep 500 or ctrl+c exit, count:$count"
+  sleep 500
+  mpoolCur=$(./health.sh chain-mpool $addr)
   if [ $mpoolCur -gt 2 ]; then
-     echo "basefee is too hight, try rise the gas"
-     ./lotus.sh mpool stat --local
-     ./lotus.sh mpool fstar-fix --really-do-it --limit-msg=0 $addr
+      count=0
+      echo "basefee is too hight, try rise the gas"
+      ./lotus.sh mpool stat --local
+      if [ $mpoolCur -gt 90 ]; then
+      	#./lotus.sh mpool fstar-fix --only-feecap=false --rate-feecap=20000 --rate-limit=10000 --limit-feecap=0.00000001FIL --max-gas=10FIL --limit-msg=30 $addr
+      	./lotus.sh mpool fstar-fix --only-feecap=false --rate-feecap=20000 --rate-limit=10000 --limit-feecap=0.00000001FIL --max-gas=10FIL --limit-msg=90 --really-do-it $addr
+      elif [ $mpoolCur -gt 20 ]; then
+      	#./lotus.sh mpool fstar-fix --only-feecap=false --rate-feecap=20000 --rate-limit=10000 --limit-feecap=0.00000001FIL --max-gas=10FIL --limit-msg=10 $addr
+      	./lotus.sh mpool fstar-fix --only-feecap=false --rate-feecap=20000 --rate-limit=10000 --limit-feecap=0.00000001FIL --max-gas=10FIL --limit-msg=10 --really-do-it $addr
+      else
+      	#./lotus.sh mpool fstar-fix --only-feecap=false --rate-feecap=20000 --rate-limit=10000 --limit-feecap=0.00000001FIL --max-gas=10FIL --limit-msg=2 $addr
+      	./lotus.sh mpool fstar-fix --only-feecap=false --rate-feecap=20000 --rate-limit=10000 --limit-feecap=0.00000001FIL --max-gas=10FIL --limit-msg=2 --really-do-it $addr
+      fi
   fi
 done
