7419734031c293ecd0785cef0a18acd87e33652c
free1139 <free1139@163.com>
2023-06-30 09:52:52 +0000

    fix pb-cli

--- apps/lotus/pb-cli.sh
+++ apps/lotus/pb-cli.sh
@@ -10,11 +10,15 @@ case $1 in
         fileName=$4
         dealStage=$5
         echo "fetch: $propCid,$fullUri,$fileName,$dealStage"
-        curl -o $dealStage $fullUri 
+        curl -s -o $dealStage $fullUri 
     ;;
     "confirm"):
+        propCid=$2
+        fullUri=$3
+        fileName=$4
+        dealStage=$5
         echo "confirm: $propCid,$fullUri,$fileName,$dealStage"
-        curl -d "propCid=$propCid&remoteUrl=$fullUri" "http://127.0.0.1:9080/deal/confirm"
+        curl -s -d "propCid=$propCid&remoteUrl=$fullUri" "http://127.0.0.1:9080/deal/confirm"
     ;;
     *)
         echo "TODO"
